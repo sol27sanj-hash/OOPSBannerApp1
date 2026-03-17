cb21628d07889dcdb0e2a23e61b3d57e6d1bd189
sol27sanj-hash <sol27sanj@gmail.com>
2026-03-17 13:57:16 +0530

    fouth usecase

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,13 +1,15 @@
-public class OOPSBannerApp {
+
+public class OOPSBanner {
 
     public static void main(String[] args) {
 
-        System.out.println(String.join(" ", "   *     ", "    *     ", "  * * * ", "    * *"));
-        System.out.println(String.join(" ", " *   *    ", " *   *    ", " *     * ", " *   "));
-        System.out.println(String.join(" ", "*     *   ", "*     *   ", " *     * ", "   * "));
-        System.out.println(String.join(" ", "*     *   ", "*     *   ", " *  *  *   ", "   *   "));
-        System.out.println(String.join(" ", "*     *   ", "*     *   ", " *      ", "       *  "));
-        System.out.println(String.join(" ", " *   *    ", " *   *    ", " *      ", "  * * *   "));
-        System.out.println(String.join(" ", "   *     ", "    *     ", "  *      ", "  "));
+        System.out.println("  *****  " + "  *****  " + " ********" + "  ****** ");
+        System.out.println(" **   ** " + " **   ** " + " **     *" + " **     *");
+        System.out.println("**     **" + "**     **" + " **      *" + "**       ");
+        System.out.println("**     **" + "**     **" + " ******** " + "  ****** ");
+        System.out.println("**     **" + "**     **" + " **       " + "       **");
+        System.out.println(" **   ** " + " **   ** " + " **      " + "*      **");
+        System.out.println("  *****  " + "  *****  " + " **      " + " ******  ");
+
     }
 }
